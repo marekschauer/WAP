db7c9fb4bd4c6a9a3b77935fdde88b456d83e68e
Marek Schauer <marek.schauer@psoit.sk>
2019-02-21 19:43:02 +0100

    String, number and select search implemented

--- js/filter.js
+++ js/filter.js
@@ -14,6 +14,23 @@ function createElementWAttr(element, attributes) {
 	return newElement;
 }
 
+/**
+ * Gets the selected options of given select.
+ *
+ * @param      HTMLElement  select  The select to be viewed
+ * @return     Array	    The array of selected options.
+ */
+function getSelectedOptionsValues(select) {
+	toBeReturned = [];
+	for (var i = 0; i < select.options.length; i++) {
+		if (select.options[i].selected) {
+			toBeReturned.push(select.options[i].value);
+		}
+	}
+	console.log(toBeReturned);
+	return toBeReturned;
+}
+
 function globalSearchHandler(e, dataToBeFiltered, inputs) {
 	// console.log('************************');
 	// console.log(e);
@@ -22,6 +39,8 @@ function globalSearchHandler(e, dataToBeFiltered, inputs) {
 
 	tmpDataToBeFiltered = dataToBeFiltered;
 
+	console.log(dataToBeFiltered);
+	console.log('~~~~~~~~~~~~');
 	// Setting all items visible before we start to hide them
 	tmpDataToBeFiltered.forEach(function(item) {
 		item.visible = true;
@@ -35,10 +54,10 @@ function globalSearchHandler(e, dataToBeFiltered, inputs) {
 			if (item.getAttribute('type') == 'text') {
 				tmpDataToBeFiltered = strSearchHandler(e, tmpDataToBeFiltered, item);
 			} else if (item.getAttribute('type') == 'number' && item.getAttribute('data-from-value') === 'true') {
-				// tmpDataToBeFiltered = numberSearchHandler(e, tmpDataToBeFiltered, item);
+				tmpDataToBeFiltered = numberSearchHandler(e, tmpDataToBeFiltered, item);
 			}
 		} else if (item.tagName == 'SELECT') {
-			// call selectSearchHandler
+			selectSearchHandler(e, tmpDataToBeFiltered, item);
 		}
 		// console.log('===');
 		// console.log(item);
@@ -75,12 +94,54 @@ function strSearchHandler(e, dataToBeFiltered, input) {
 	return dataToBeFiltered;
 }
 
+function numberSearchHandler(e, dataToBeFiltered, input) {
+	interval = {
+		'from': Number.MIN_SAFE_INTEGER,
+		'to': Number.MAX_SAFE_INTEGER
+	};
+	inputFrom = input.value;
+	interval.from = input.value === "" ? Number.MIN_SAFE_INTEGER : parseInt(input.value,10);
+	inputId = input.getAttribute('id');
+	toValue = document.getElementById(inputId.substr(0, inputId.length-5) + '_to').value;
+	interval.to = toValue === "" ? Number.MAX_SAFE_INTEGER : parseInt(toValue, 10);
+	
+
+	dataToBeFiltered.forEach(function(item) {
+
+		toBeFiltered = item.node.querySelector('.' + input.getAttribute('data-property-name'));
+		
+		if (item.visible && parseInt(toBeFiltered.innerHTML, 10) >= parseInt(interval.from, 10) && parseInt(toBeFiltered.innerHTML, 10) <= parseInt(interval.to, 10)) {
+			item.node.style.display = 'initial';
+			item.visible = true;
+		} else {
+			item.node.style.display = 'none';
+			item.visible = false;
+		}
+
+	});
+
+	return dataToBeFiltered;
+}
 
 
-function selectSearchHandler(e, dataToBeFiltered) {
-	for (var i = this.selectedOptions.length - 1; i >= 0; i--) {
-		console.log(this.selectedOptions[i].value);
-	}
+
+function selectSearchHandler(e, dataToBeFiltered, select) {
+	selectedOptions = getSelectedOptionsValues(select);
+	console.log(selectedOptions);
+	// selectedOptions.forEach(function(item) {
+	// 	console.log(item.value);
+	// });
+
+	dataToBeFiltered.forEach(function(item) {
+		toBeFiltered = item.node.querySelector('.' + select.getAttribute('data-property-name'));
+		if (item.visible && selectedOptions.includes(toBeFiltered.innerHTML)) {
+			item.node.style.display = 'initial';
+			item.visible = true;
+		} else {
+			item.node.style.display = 'none';
+			item.visible = false;
+		}
+	});
 }
 
 /**
@@ -100,7 +161,10 @@ function initFilter(paramsObj) {
 			'visible': true
 		});
 	}
+
+	console.log('-------');
 	console.log(filtered);
+	console.log('-------');
 
 	if (paramsObj.filterable.length > 0) {
 		form = createElementWAttr('form', {});
@@ -115,7 +179,8 @@ function initFilter(paramsObj) {
 					'element': createElementWAttr('input', {
 															'type': 'text',
 															'id': paramsObj.id + '_' + currentFilter.name,
-															'data-property-name': currentFilter.name
+															'data-property-name': currentFilter.name,
+															'placeholder': currentFilter.form_placeholder
 														}),
 					'callback': strSearchHandler,
 					'dataToBeFiltered': filtered
@@ -126,7 +191,8 @@ function initFilter(paramsObj) {
 				tmpElements.push({'element': createElementWAttr('input', {
 											'type': 'number',
 											'id': paramsObj.id + '_' + currentFilter.name + '_to',
-											'data-property-name': currentFilter.name
+											'data-property-name': currentFilter.name,
+											'placeholder': currentFilter.form_placeholder[1]
 										}),
 									'callback': strSearchHandler,
 									'dataToBeFiltered': filtered
@@ -137,6 +203,7 @@ function initFilter(paramsObj) {
 											'id': paramsObj.id + '_' + currentFilter.name + '_from',
 											'data-property-name': currentFilter.name,
 											'data-from-value': 'true',
+											'placeholder': currentFilter.form_placeholder[0]
 										}),
 									'callback': strSearchHandler,
 									'dataToBeFiltered': filtered
@@ -147,7 +214,7 @@ function initFilter(paramsObj) {
 					'multiple': 'true',
 					'name': paramsObj.id + '_' + currentFilter.name + '[]',
 					'id': paramsObj.id + '_' + currentFilter.name,
-					'data-property-name': currentFilter.name
+					'data-property-name': currentFilter.name,
 				});
 
 				selected = document.querySelectorAll("#" + paramsObj.id + " .item ." + currentFilter.name);
@@ -162,7 +229,11 @@ function initFilter(paramsObj) {
 						uniqueOptions.push(currentValue.innerHTML);
 					}
 				});
-				tmpElements.push({'element': tmpEl, 'callback': selectSearchHandler});
+				tmpElements.push({
+					'element': tmpEl,
+					'callback': selectSearchHandler,
+					'dataToBeFiltered': filtered
+				});
 			}
 			
 			tmpElements.forEach(function(elem) {
@@ -183,22 +254,27 @@ initFilter({
 		{
 			'name': 'title',
 			'type': 'str',
+			'form_placeholder': 'Nadpis'
 		},
 		{
 			'name': 'price',
-			'type': 'num'
+			'type': 'num',
+			'form_placeholder': ['Cena od', 'Cena do']
 		},
 		{
 			'name': 'author',
-			'type': 'str'
+			'type': 'str',
+			'form_placeholder': 'Autor',
 		},
 		{
 			'name': 'quantity',
-			'type': 'num'
+			'type': 'num',
+			'form_placeholder': ['Počet od', 'Počet do']
 		},
 		{
 			'name': 'publisher',
-			'type': 'str'
+			'type': 'enumeration',
+			'form_placeholder': 'Vydavateľstvo'
 		},
 	]
 });
